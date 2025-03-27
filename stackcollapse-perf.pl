9db7cfd33329274d9515d750e834aa80ceb2f405
Jordan Han <jordan.han@htuobio.com>
2025-03-27 13:08:57 -0700

    update

--- stackcollapse-perf.pl
+++ stackcollapse-perf.pl
@@ -193,6 +193,32 @@ sub inline {
 	return $result;
 }
 
+sub trim {
+    my $str = shift;
+    my $depth_angle = 0;
+    
+	# Find first parentheses that is outside of angle brackets
+    for (my $i = 0; $i < length($str); $i++) {
+        my $char = substr($str, $i, 1);
+        
+        if ($char eq '<') {
+            $depth_angle++;
+        }
+
+        elsif ($char eq '>') {
+            $depth_angle-- if $depth_angle > 0;
+        }
+
+        elsif ($char eq '(' && $depth_angle == 0) {
+			my $parentheses_part = substr($str, $i);
+			$parentheses_part =~ s/\((?!anonymous namespace\)).*//;
+			return substr($str, 0, $i) . $parentheses_part;
+        }
+    }
+    
+    return $str;  # No matching parentheses found
+}
+
 my @stack;
 my $pname;
 my $m_pid;
@@ -348,7 +374,8 @@ while (defined($_ = <>)) {
 					# "net/http.(*Client).Do"), so everything after the first open
 					# paren (that is not part of an "(anonymous namespace)") is
 					# just noise.
-					$func =~ s/\((?!anonymous namespace\)).*//;
+
+					$func =~ trim($func);
 				}
 				# now tidy this horrible thing:
 				# 13a80b608e0a RegExp:[&<>\"\'] (/tmp/perf-7539.map)
